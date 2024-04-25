c27f5c9d74ca51d51e79957337959c482381c4fc
Gabe Hollifield <94938857+gabholli@users.noreply.github.com>
2024-04-25 12:41:24 -0700

    Added back-to-top button in projects page

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const backToTopButton = document.getElementById("back-to-top-button")
 
+// Function to handle the "scroll to top" button click
+const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'  // for smoothly scrolling
+    });
+};
+
+
+backToTopButton.addEventListener('click', scrollToTop)
